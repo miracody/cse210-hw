90b7b80a8335a2763546e0c5856ac72770f5247c
Mirabel Nwanneka <memeruwa@gmail.com>
2025-11-27 19:22:17 +0100

    work

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -7,22 +7,16 @@ class Program
         // Test base class
         Assignment simple = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(simple.GetSummary());
-        // Output: Samuel Bennett - Multiplication
-
+        
         // Test MathAssignment
         MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(math.GetSummary());
         Console.WriteLine(math.GetHomeworkList());
-        // Output:
-        // Roberto Rodriguez - Fractions
-        // Section 7.3 Problems 8-19
+        
 
         // Test WritingAssignment
         WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(writing.GetSummary());
         Console.WriteLine(writing.GetWritingInformation());
-        // Output:
-        // Mary Waters - European History
-        // The Causes of World War II by Mary Waters
     }
 }
